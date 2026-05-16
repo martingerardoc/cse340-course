e0e7659ee38c47dbc2d24e9ee7eacf7942ba202a
martingerardoc <martingerardoc56@gmail.com>
2026-05-16 08:50:51 -0300

    DATABASE_URL changed

--- src/models/db.js
+++ src/models/db.js
@@ -12,7 +12,7 @@ import { Pool } from 'pg';
  * postgresql://username:password@host:port/database
  */
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
     rejectUnauthorized: false
 }
